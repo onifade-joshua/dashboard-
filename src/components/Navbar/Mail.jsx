4542ae3a3c16fe6658efcaabd5c55b66d1533a72
onifade-joshua <onifadejoshua15@gmail.com>
2022-12-05 08:53:45 +0100

    first commit

---
+++ src/components/Navbar/Mail.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Mail = () => {
+  return (
+    <>
+    <div className="menu">
+        <ul>
+            <li> <Link to="/"></Link></li>
+            <li> <Link to="/"></Link></li>
+            <li> <Link to="/"></Link></li>
+        </ul>
+    </div>
+    <Routes-links></Routes-links>
+    </>
+  )
+}
+
+export default Mail;
